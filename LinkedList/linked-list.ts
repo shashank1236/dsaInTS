3674dfa5cd2f29385633d2fdef9b926839232b1e
shashank mittal <shashankmittal201@gmail.com>
2025-02-19 01:10:06 +0530

    set method

--- LinkedList/linked-list.ts
+++ LinkedList/linked-list.ts
@@ -1,6 +1,6 @@
 import { LLNode } from "./node";
 
-class LinkedList {
+export class LinkedList {
   head: LLNode;
   tail: LLNode;
   length: number;
@@ -63,7 +63,7 @@ class LinkedList {
    * @param value The new value to be added to the linked list.
    * @returns The updated linked list.
    */
-  unshift(value: string | number) {
+  unshift(value: string | number): LinkedList {
     const newNode = new LLNode(value);
     // if we have zero elements in node
     if (!this.head) {
@@ -77,17 +77,56 @@ class LinkedList {
   }
 
   /**
+   * Removes the first node in the linked list.
    * 
+   * @returns The item removed from the linked list.
    */
-  shift() {
+  shift(): LLNode {
     if (!this.head) return undefined
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--
+    if (this.length === 0) this.tail = null
+    return temp
+  }
 
+  /**
+   * Returns the node at the specified index in the linked list.
+   *
+   * @param index The index of the node to be returned.
+   * @returns The node at the specified index in the linked list.
+   */
+  get(index: number): LLNode | string {
+    if (index < 0 || index >= this.length) return "index is greater than length of linked list"
+    let current = this.head;
+    let i = 0;
+    while (i < index) {
+      current = current.next
+      i++
+    }
+    return current
+  }
+
+  set(index: number, value: string) {
+    if (index < 0 || index >= this.length) return "index is greater than length of linked list"
+    let current = this.head;
+    let i = 0;
+    while (i < index) {
+      current = current.next
+      i++
+    }
+    if (current) {
+      current.value = value
+      return true
+    }
+    return
   }
 }
 
 let ll = new LinkedList(7);
-// ll.push(4)
-// ll.unshift("first")
-ll.pop()
-ll.shift()
+ll.push(4)
+ll.push(23)
+ll.push(45)
+ll.set(3, "shashank")
 console.log(ll)
